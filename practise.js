b015c8fea0f367125268c55530b556354645cdd4
ayush2047 <113309059+ayush2047@users.noreply.github.com>
2024-04-14 14:56:02 +0530

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,66 @@
+/*
+function add(a,b){
+    return a+b; 
+};
+var result = add(2,3);
+console.log(result);
+*/
+/*
+var add= function(a,b){
+    return a+b;
+}
+*/
+//var add=(a,b)=>{return a+b};
+
+//var add=(a,b)=>a+b;
+/*
+(function(){
+    console.log('ayush')
+})();
+
+var result = add(2,3);
+console.log(result);
+*/
+
+/*
+json string to object
+const jsonstring='{"name":"John","age":25,"city":"new york"}';
+const jsonObject=JSON.parse(jsonstring);
+console.log(jsonObject.name);
+*/
+
+/*
+const obj={
+    name:"Alice",
+    age:25
+};
+const jsonstringified = JSON.stringify(obj);//convert obj to json string
+console.log(jsonstringified);
+console.log(typeof jsonstringified)
+*/
+
+
+/*
+var prompt=require('prompt-sync')();
+const age= prompt("enter age");
+if(age<18){
+    console.log("can't vote")
+}
+else if(age>18 && age<=65){
+
+    console.log("normal")
+}
+
+else{
+    console.log("can")
+}
+*/
+
+/*
+var prompt=require('prompt-sync')();
+const length=prompt=("enter length");
+
+const width=prompt=("enter width");
+var area= length*width;
+console.log(area)
+*/
